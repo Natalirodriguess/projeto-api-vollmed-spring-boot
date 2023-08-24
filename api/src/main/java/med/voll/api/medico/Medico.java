84846bd90824f9ae7ad38e3922c525c38f48aac4
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-24 16:04:13 -0300

    [Feature-002] Adicionando entidades e interface Respository para salvar dados recebidos nos request body para a API

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -1,5 +1,5 @@
 package med.voll.api.medico;
-
+//entidade que representa o objeto Medico na tabela medicos
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -28,4 +28,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    public Medico(DadosCadastroMedicos dados) {
+        this.nome = dados.nome();
+        this.crm = dados.crm();
+        this.especialidade = dados.especialidade();
+        this.endereco = new Endereco(dados.endereco());
+    }
 }

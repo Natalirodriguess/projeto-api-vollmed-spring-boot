8a5ac8c41ecded2dcfc836fd564e76f5d7fc0bfe
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-26 23:24:42 -0300

    [feature-002] Adicionando método GET para o recurso médicos

--- api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosListagemMedico.java
@@ -1,8 +1,15 @@
 package med.voll.api.medico;
 
+<<<<<<< HEAD
 public record DadosListagemMedico(Long id, String nome, String email, String crm, Especialidade especialidade) {
 
     public DadosListagemMedico(Medico medico){
         this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+=======
+public record DadosListagemMedico(String nome, String email, String crm, Especialidade especialidade) {
+
+    public DadosListagemMedico(Medico medico){
+        this(medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getEspecialidade());
+>>>>>>> 068b100 ([feature-002] Adicionando método GET para o recurso médicos)
     }
 }

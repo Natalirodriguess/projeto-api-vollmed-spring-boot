19e56289d444556068c37bf5c5c4a0404b2176da
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-27 23:52:38 -0300

    [feature-002] Adicionando Pageable default

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -8,11 +8,11 @@ import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -27,7 +27,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(Pageable paginacao){
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 3, sort = "nome" ) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 

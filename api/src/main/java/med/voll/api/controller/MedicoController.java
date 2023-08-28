3033d8c97c1c36a81ea34fcfdf8f4ebcb164107a
Natalí Rodrigues <natalyrodrigues43210@gmail.com>
2023-08-27 22:59:35 -0300

    [feature-002] Tornando o retorno do método buscar médicos em um pageable

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,17 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.medico.DadosCadastroMedicos;
+import med.voll.api.medico.DadosListagemMedico;
+import med.voll.api.medico.Medico;
+import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
-
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,7 +26,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar(@PageableDefault(size = 3, sort = "nome" ) Pageable paginacao){
+    public Page<DadosListagemMedico> listar(Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
